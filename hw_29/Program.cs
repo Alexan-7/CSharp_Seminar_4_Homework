acc87ba7fe4168af0dfecbfcfaafbf593f9b9f74
Sasha <brdgn@bk.ru>
2022-09-13 22:52:36 +0300

    Third commit

--- hw_29/Program.cs
+++ hw_29/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Введите 8 чисел через пробел");
 string abcdefgh = Console.ReadLine()!;
 string[] parts = abcdefgh.Split(' ');
 
-int [] CreateArray (int size)
+int [] CreateArray (int size) // функция созания массива
 {    
     int[] array = new int[size];  // объявляем массив
     for (int i = 0; i < size; i++)  // сделать 8 раз  (для i от 0 до 7)
@@ -14,7 +14,7 @@ int [] CreateArray (int size)
     return array;
 }
 
-void PrintArray (int[] arra)
+void PrintArray (int[] arra) // функция вывода массива на экран
 {
     Console.Write($"[{string.Join(", ", arra)}]");
 }
